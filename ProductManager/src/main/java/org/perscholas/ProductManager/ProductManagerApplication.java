f765fab8a97383aee934eab3d6a41d459bd6d056
shravani padma <shravani.padma45@gmail.com>
2020-03-04 10:08:32 -0600

    Comit

---
+++ ProductManager/src/main/java/org/perscholas/ProductManager/ProductManagerApplication.java
@@ -0,0 +1,15 @@
+package org.perscholas.ProductManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerApplication.class, args);
+	}
+
+}
+//check the output in localhost:8080 in browser
+
---
+++ ProductManager/src/main/java/org/perscholas/ProductManager/ProductRepository.java
@@ -0,0 +1,7 @@
+package org.perscholas.ProductManager;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer>{
+
+}
